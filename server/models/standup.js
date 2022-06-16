62e1920edc6cca4439870c1c7d8216fc1ade439b
chrisstophe <christophe.rezk@gmail.com>
2022-06-16 15:54:25 -0400

    Added custom validators

--- server/models/standup.js
+++ server/models/standup.js
@@ -1,15 +1,37 @@
 const mongoose = require("mongoose");
 
+// Validator function
+const requiredStringValidator = [
+  function (val) {
+    let testVal = val.trim();
+    return testVal.length > 0;
+  },
+  // Custom error text
+  "Please supply a value for {PATH}",
+];
+
 const standupSchema = new mongoose.Schema({
   _teamMemberId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "teammembers",
   },
-  teamMember: { type: String },
-  project: { type: String },
-  workYesterday: { type: String },
-  workToday: { type: String },
-  impediment: { type: String },
+  teamMember: {
+    type: String,
+    required: true,
+    validate: requiredStringValidator,
+  },
+  project: { type: String, required: true, validate: requiredStringValidator },
+  workYesterday: {
+    type: String,
+    required: true,
+    validate: requiredStringValidator,
+  },
+  workToday: {
+    type: String,
+    required: true,
+    validate: requiredStringValidator,
+  },
+  impediment: { type: String, required: true, default: "None" },
   createdOn: { type: Date, default: Date.now() },
 });
 
